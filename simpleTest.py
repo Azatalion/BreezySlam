cc791cc83cfc5602d5b6d674222ea70610b1f845
Azatalion <azatalion@gmail.com>
2019-10-11 20:53:42 +0400

    Update simpleTest.py

--- simpleTest.py
+++ simpleTest.py
@@ -107,14 +107,14 @@ while vrep.simxGetConnectionId(clientID) != -1:
 
     #считаем одометрию для обоих колес
     errorCode, x_left = vrep.simxGetJointPosition(clientID, left_motor_handle, vrep.simx_opmode_streaming)
-    dx_left = abs(x_left - prev_pos_left)
+    dx_left = x_left - prev_pos_left
     prev_pos_left = x_left
     if dx_left >= 0:
         dx_left = (dx_left + math.pi) % (2 * math.pi) - math.pi
     else:
         dx_left = (dx_left - math.pi) % ( 2 * math.pi) + math.pi
     errorCode, x_right = vrep.simxGetJointPosition(clientID, right_motor_handle, vrep.simx_opmode_streaming)
-    dx_right = abs(x_right - prev_pos_right)
+    dx_right = x_right - prev_pos_right
     prev_pos_right = x_right
     if dx_right >= 0:
         dx_right = (dx_right + math.pi) % (2 * math.pi) - math.pi
